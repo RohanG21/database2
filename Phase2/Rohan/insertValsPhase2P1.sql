a9f8581e457170ad02a292d291ede18f5ef278a5
RohanG21 <96847964+RohanG21@users.noreply.github.com>
2025-03-23 03:39:33 -0400

    all students added for p1, corrections made for ta ids, total students: 67

--- Phase2/Rohan/insertValsPhase2P1.sql
+++ Phase2/Rohan/insertValsPhase2P1.sql
@@ -21,43 +21,75 @@ INSERT INTO student VALUES(111,"Jack Thomas","jackthomas@student.uml.edu","Chemi
 INSERT INTO student VALUES(112,"Chelsea Melise","chelseamelise@student.uml.edu","Chemistry");
 INSERT INTO student VALUES(113,"Kevin Navik", "kevinnavik@student.uml.edu","Chemistry");
 INSERT INTO student VALUES(114,"Lauren Evij","laurenevij@student.uml.edu","Chemistry");
-INSERT INTO student VALUES(115,"Warren Nicholson","warrennicholson@student.uml.edu","Miner School Of Computer & Information Sciences");
-INSERT INTO student VALUES(116,"Eliz Gomez","elizgomez@student.uml.edu","Miner School Of Computer & Information Sciences");
-INSERT INTO student VALUES(117,"Ted Baven","tedbaven@student.uml.edu","Mathematics & Statistics");
-INSERT INTO student VALUES(118,"Yanike Shoto","yanikeshoto@student.uml.edu","Mechanical Engineering & Industrial Engineering");
-INSERT INTO student VALUES(119,"Urevi Krevk","urevikrevk@student.uml.edu","Miner School Of Computer & Information Sciences");
-INSERT INTO student VALUES(120,"Ingei Tase ","ingeitase@student.uml.edu","Solomont School Of Nursing");
-INSERT INTO student VALUES(121,"Obe Hano", "obehano@student.uml.edu","Chemistry");
-INSERT INTO student VALUES(122,"Peter Daven","peterdaven@student.uml.edu","Miner School Of Computer & Information Sciences");
-INSERT INTO student VALUES(123,"Aven Menre","avenmenre@student.uml.edu","Miner School Of Computer & Information Sciences");
-INSERT INTO student VALUES(124,"Samuel Naten","samuelnaten@student.uml.edu","Miner School Of Computer & Information Sciences");
-INSERT INTO student VALUES(125,"Feren Telal","ferentelal@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(115,"Zain Eman","zaineman@student.uml.edu","Chemistry");
+INSERT INTO student VALUES(116,"Warren Nicholson","warrennicholson@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(117,"Eliz Gomez","elizgomez@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(118,"Ted Baven","tedbaven@student.uml.edu","Mathematics & Statistics");
+INSERT INTO student VALUES(119,"Yanike Shoto","yanikeshoto@student.uml.edu","Mechanical Engineering & Industrial Engineering");
+INSERT INTO student VALUES(120,"Urevi Krevk","urevikrevk@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(121,"Geia Jani", "geiajani@student.uml.edu","Mechanical Engineering & Industrial Engineering");
+INSERT INTO student VALUES(122,"Ingei Tase ","ingeitase@student.uml.edu","Solomont School Of Nursing");
+INSERT INTO student VALUES(123,"Obe Hano", "obehano@student.uml.edu","Chemistry");
+INSERT INTO student VALUES(124,"Peter Daven","peterdaven@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(125,"Aven Menre","avenmenre@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(126,"Samuel Naten","samuelnaten@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(127,"Krei Ane","kreiane@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(128,"Feren Telal","ferentelal@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(129,"Gary Hamsen","garyhamsen@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(130,"Helena Cndroi","helenacndroi@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(131,"Iman Khalil","imankhalil@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(132,"Jania Namley","janianamley@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(133,"Rha Zaln","rhazaln@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(134,"Kanin Tolov","kanintolov@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(135,"Laila Bani","lailabani@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(136,"Zain Anlo","zainanlo@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(137,"Xavia Valen","xaviavalen@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(138,"Connor Henson","connorhenson@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(139,"Famei Shen","fameishen@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(140,"Vaya Siereo","vayasiereo@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(141,"Beni Aka","beniaka@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(142,"Nyla Hilson","nylahilson@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(143,"Maria Enez","mariaenez@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(144,"Quen Kaian","quenkaian@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(145,"Wei Cheng","weicheng@student.uml.edu","Miner School Of Computer & Information Sciences");
+INSERT INTO student VALUES(146,"Emmane Haub","emmanehaub@student.uml.edu","Mathematics & Statistics");
+INSERT INTO student VALUES(147,"Rasin Hanan","rasinhanan@student.uml.edu","Mathematics & Statistics");
+INSERT INTO student VALUES(148,"Tayl Stelan","taylstelan@student.uml.edu","Mechanical Engineering & Industrial Engineering");
+INSERT INTO student VALUES(149,"Yen Hito","yenhito@student.uml.edu","Mathematics & Statistics");
+INSERT INTO student VALUES(150,"Ueyfe Zazik","uefyezazik@student.uml.edu","Mathematics & Statistics");
+INSERT INTO student VALUES(151 "Hijan Manvn","hijanmanvn@student.uml.edu","Mathematics & Statistics");
+INSERT INTO student VALUES(152,"Ivea Volti","iveavolti@student.uml.edu","Solomont School Of Nursing");
+INSERT INTO student VALUES(153,"Oneo Gaia","oneogaia@student.uml.edu","Solomont School Of Nursing");
+INSERT INTO student VALUES(154,"Paris Nicela","parisnicela@student.uml.edu","Solomont School Of Nursing");
+INSERT INTO student VALUES(155,"Aalie Shannon","aalieshannoni@student.uml.edu","Solomont School Of Nursing");
+INSERT INTO student VALUES(156,"Shayla Cyre","shaylacyre@student.uml.edu","Solomont School Of Nursing");
+INSERT INTO student VALUES(157,"Lanley Vensen", "lanleyvensen@student.uml.edu","Solomont School Of Nursing");
 
 
 
-INSERT INTO student VALUES(106,'Matt Fensu','mattfensu@student.uml.edu','Miner School Of Computer And Information Sciences');
-INSERT INTO student VALUES(107,'Neai Boko','neaiboko@student.uml.edu','Mechanical & Industrial Engineering');
-INSERT INTO student VALUES(108,'Chi Deng', 'chideng@student.uml.edu','Chemistry');
-INSERT INTO student VALUES(109,'Asam Setan','asamsetan@student.uml.edu','Mathematics & Statistics');
-INSERT INTO student VALUES(110,'Christopher Samuels','christophersamuels@student.uml.edu','Solomont School Of Nursing');
+INSERT INTO student VALUES(158,'Matt Fensu','mattfensu@student.uml.edu','Miner School Of Computer And Information Sciences');
+INSERT INTO student VALUES(159,'Neai Boko','neaiboko@student.uml.edu','Mechanical & Industrial Engineering');
+INSERT INTO student VALUES(160,'Chi Deng', 'chideng@student.uml.edu','Chemistry');
+INSERT INTO student VALUES(161,'Asam Setan','asamsetan@student.uml.edu','Mathematics & Statistics');
+INSERT INTO student VALUES(162,'Christopher Samuels','christophersamuels@student.uml.edu','Solomont School Of Nursing');
 
 INSERT INTO instructor VALUES(6,'Ronald Brent','Teaching Professor','Mathematics & Statistics', 'Ronald_Brent@uml.edu');
 INSERT INTO instructor VALUES(7,'Robert Parkin','Teaching Professor','Mechanical & Industrial Engineering','Robert_Parkin@uml.edu');
 INSERT INTO instructor VALUES(8,'Suri Iyer','Teaching Professor','Chemistry','suri_iyer@uml.edu');
 INSERT INTO instructor VALUES(9,'Ainat Koren','Teaching Professor','Solomont School Of Nursing', 'Ainat_Koren@uml.edu');
 
-INSERT INTO phd VALUES('106',1,20230516,20250516);
-INSERT INTO phd VALUES('107',1,20220925,20260312);
-INSERT INTO phd VALUES('108',0,NULL,NULL);
-INSERT INTO phd VALUES('109',1,20200208,20250406);
-INSERT INTO phd VALUES('110',1,20241103,20280119);
+INSERT INTO phd VALUES('158',1,20230516,20250516);
+INSERT INTO phd VALUES('159',1,20220925,20260312);
+INSERT INTO phd VALUES('160',0,NULL,NULL);
+INSERT INTO phd VALUES('161',1,20200208,20250406);
+INSERT INTO phd VALUES('162',1,20241103,20280119);
 
 
-INSERT INTO advise VALUES(1,'106',20230516,20250516);
-INSERT INTO advise VALUES(6,'109',20200208,20250406);
-INSERT INTO advise VALUES(7,'107',20230903,20260312);
-INSERT INTO advise VALUES(8,'108',20250220, NULL);
-INSERT INTO advise VALUES(9,'110',20241108,20280119);
+INSERT INTO advise VALUES(1,'158',20230516,20250516);
+INSERT INTO advise VALUES(6,'159',20200208,20250406);
+INSERT INTO advise VALUES(7,'160',20230903,20260312);
+INSERT INTO advise VALUES(8,'161',20250220, NULL);
+INSERT INTO advise VALUES(9,'162',20241108,20280119);
 
 
 INSERT INTO course VALUES('MATH2310','Calculus III',4);
@@ -67,11 +99,11 @@ INSERT INTO course VALUES('PUBH2010','Community Health And Evironment', 3);
 INSERT INTO course VALUES('PHYS1410','Physics I', 3);
 INSERT INTO course VALUES('MATH1320','Calculus II',4);
 
-INSERT INTO student VALUES('111','Yvonne Asein','yvonneasein@student.uml.edu','Chemistry');
-INSERT INTO student VALUES('112','Isabella Matova','isabellamatova@student.uml.edu','Mechanical & Industrial Engineering');
-INSERT INTO student VALUES('113','Oliver Danton','oliverdanton@student.uml.edu','Mathematics & Statistics');
-INSERT INTO student VALUES('114','Priya Nagu','priyanagu@student.uml.edu','Solomont School Of Nursing');
-INSERT INTO student VALUES('115','Andes Rofello','andesrofello@student.uml.edu','Miner School Of Computer And Information Sciences');
+INSERT INTO student VALUES('163','Yvonne Asein','yvonneasein@student.uml.edu','Chemistry');
+INSERT INTO student VALUES('164','Isabella Matova','isabellamatova@student.uml.edu','Mechanical & Industrial Engineering');
+INSERT INTO student VALUES('165','Oliver Danton','oliverdanton@student.uml.edu','Mathematics & Statistics');
+INSERT INTO student VALUES('166','Priya Nagu','priyanagu@student.uml.edu','Solomont School Of Nursing');
+INSERT INTO student VALUES('167','Andes Rofello','andesrofello@student.uml.edu','Miner School Of Computer And Information Sciences');
 
 INSERT INTO section VALUES('CHEM1210','Section100','Spring','2025',8,NULL,'TS1')
 
@@ -116,11 +148,11 @@ INSERT INTO undergraduate('Sophomore',102,28);
 INSERT INTO undergraduate('Freshman',103,28);
 INSERT INTO undergraduate('Sophomore',104,28);
 
-INSERT INTO master(111,15)
-INSERT INTO master(112,15)
-INSERT INTO master(113,15)
-INSERT INTO master(114,15)
-INSERT INTO master(115,23)
+INSERT INTO master(163,15)
+INSERT INTO master(164,15)
+INSERT INTO master(165,15)
+INSERT INTO master(166,15)
+INSERT INTO master(167,23)
 
 INSERT INTO undergraduategrader('COMP1020','Section101','Spring',100,2024)
 INSERT INTO undergraduategrader('CHEM1210','Section100','Spring',100,2025)
@@ -128,17 +160,17 @@ INSERT INTO undergraduategrader('MATH2310','Section100','Spring',100,2025)
 INSERT INTO undergraduategrader('PUBH2010','Section100','Spring',102,2025)
 INSERT INTO undergraduategrader('ENGN2050','Section100','Fall',100,2024)
 
-INSERT INTO mastergrader('CHEM1210','Section100','Spring',111,2025)
-INSERT INTO mastergrader('MATH2310','Section100','Spring',113,2025)
-INSERT INTO mastergrader('ENGN2050','Section100','Fall',112,2024)
-INSERT INTO mastergrader('PUBH2010','Section100','Spring',114,2025)
-INSERT INTO mastergrader('COMP1020','Section101','Spring',115,2024)
+INSERT INTO mastergrader('CHEM1210','Section100','Spring',163,2025)
+INSERT INTO mastergrader('MATH2310','Section100','Spring',164,2025)
+INSERT INTO mastergrader('ENGN2050','Section100','Fall',165,2024)
+INSERT INTO mastergrader('PUBH2010','Section100','Spring',166,2025)
+INSERT INTO mastergrader('COMP1020','Section101','Spring',167,2024)
 
-INSERT INTO ta('COMP1010','Section100','Spring',106,2024)
-INSERT INTO ta('ENGN2050','Section100','Fall',107,2024)
-INSERT INTO ta('CHEM1210','Section100','Spring',108,2025)
-INSERT INTO ta('MATH2310','Section100','Spring',109,2025)
-INSERT INTO ta('PUBH2010','Section100','Spring',110,2025)
+INSERT INTO ta('COMP1010','Section100','Spring',163,2024)
+INSERT INTO ta('ENGN2050','Section100','Fall',164,2024)
+INSERT INTO ta('CHEM1210','Section100','Spring',165,2025)
+INSERT INTO ta('MATH2310','Section100','Spring',166,2025)
+INSERT INTO ta('PUBH2010','Section100','Spring',167,2025)
 
 
 
