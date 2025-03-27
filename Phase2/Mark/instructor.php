d9c03aac76a885193c1df2b878069380dd6b89fe
MarkUML <71396575+MarkUML@users.noreply.github.com>
2025-03-27 00:02:36 -0400

    Add files via upload

--- Phase2/Mark/instructor.php
+++ Phase2/Mark/instructor.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 <center>
-<h1> Instructor </h1>
+<h1> Instructor Homepage </h1>
 <details>
 	<summary><h2>View taught section information</h2></summary>
 	<details>
@@ -25,7 +25,7 @@
 		<?php
 			$query = "SELECT * FROM section WHERE instructor_id = $ins_id ORDER BY year";
 			$result = $mysqli->query($query);
-			# Displays all sections being taught or previously taught
+			// Displays all sections being taught or previously taught
 			if ($result->num_rows > 0) {
 				echo "<table border = '1' cellpadding = '4' cellspacing = '0'>";
 				echo	"<tr><th>Course ID</th>
@@ -34,8 +34,8 @@
 						<th>Year</th>
 						<th>Instructor ID</th>
 						</tr>";
-						#<th>Classroom ID</th>
-						#<th>Timeslot ID</th>";
+						//<th>Classroom ID</th>
+						//<th>Timeslot ID</th>";
 	
 			while($row = $result->fetch_assoc()) {
 				$course_id = $row['course_id'];
@@ -43,16 +43,16 @@
 				$semester = $row['semester'];
 				$year = $row['year'];
 				$inst_id = $row['instructor_id'];
-				#$room_id = $row['classroom_id'];
-				#$timeslot = $row['time_slot_id'];
+				//$room_id = $row['classroom_id'];
+				//$timeslot = $row['time_slot_id'];
 				echo "<tr>";
 				echo "<td>$course_id</td>";
 				echo "<td>$section_id</td>";
 				echo "<td>$semester</td>";
 				echo "<td>$year</td>";
 				echo "<td>$inst_id</td>";
-				#echo "<td>$room_id</td>";
-				#echo "<td>$timeslot</td>";
+				//echo "<td>$room_id</td>";
+				//echo "<td>$timeslot</td>";
 				echo "</tr>";
 			}
 			echo "</table>";
@@ -65,7 +65,7 @@
 		<summary><h3>Sections taught before Spring 2025 with names + grades of students</h3></summary>
 		<?php
 	
-		# Displays sections taught by instructor before Spring 2025 with the names + grades of students who took them
+		// Displays sections taught by instructor before Spring 2025 with the names + grades of students who took them
 		$query = "SELECT St.name, T.grade, T.section_id, T.course_id, T.semester, T.year FROM student St, take T, section Se 
 			WHERE St.student_id = T.student_id AND T.section_id = Se.section_id 
 			AND T.course_id = Se.course_id
@@ -74,7 +74,7 @@
 			ORDER BY T.year";
 		$result = $mysqli->query($query);
 		if ($result->num_rows > 0) {
-			#echo "<h2>Sections taught before Spring 2025 with names + grades of students:</h2>";
+			//echo "<h2>Sections taught before Spring 2025 with names + grades of students:</h2>";
 			echo "<table border = '1' cellpadding = '4' cellspacing = '0'>";
 			echo	"<tr><th>Course ID</th>
 					<th>Section ID</th>
@@ -110,7 +110,7 @@
 		<summary><h3>Sections taught during the current semester with the names of students</h3></summary>
 		<?php
 	
-		# Displays sections currently being taught by instructor with only the names of current enrolled students
+		// Displays sections currently being taught by instructor with only the names of current enrolled students
 		$query = "SELECT S.name, T.section_id, T.course_id, T.semester, T.year, T.grade FROM student S, take T, section Se
 			WHERE S.student_id = T.student_id AND T.course_id = Se.course_id 
 			AND T.section_id = Se.section_id AND T.semester = Se.semester
@@ -151,6 +151,9 @@
 		?>
 	</details>
 </details>
+
+<p><a href="advisor_controls.php">Manage advisor appointments and advisee info</a>.</p>
+<p><a href="index.html">Log out and return to the homepage</a>.</p>
 
 </center>
 </body>
