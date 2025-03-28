6b710dd85633ae2e2d8872642d39f96be5add0bc
MarkUML <71396575+MarkUML@users.noreply.github.com>
2025-03-27 20:40:12 -0400

    Empty admin.php to try and test task 8 functionality

---
+++ Phase2/Mark/admin.php
@@ -0,0 +1,21 @@
+<?php
+	include 'config.php';
+	session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> Test admin</title>
+</head>
+<body>
+<center>
+<h1>Test admin</h1>
+
+<p><a href="advisor_controls.php">appoint advisors</a>.</p>
+
+
+</center>
+</body>
+</html>
